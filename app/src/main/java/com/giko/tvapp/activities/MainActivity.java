6d72d2f0b9f15035a558b9c1d9c43541ae2eed31
Gideon Ukoko Jr <gideon.ukoko98@gmail.com>
2021-09-29 22:23:53 +0100

    Created ApiClient and TV Series Model classes

--- app/src/main/java/com/giko/tvapp/activities/MainActivity.java
+++ app/src/main/java/com/giko/tvapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.giko.tvapp;
+package com.giko.tvapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.giko.tvapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
